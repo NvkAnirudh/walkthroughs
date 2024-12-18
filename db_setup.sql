0987697e1d4e546e727bbb8e88ac47a9db30a3d0
NvkAnirudh <nuti.krish4@gmail.com>
2024-12-18 04:29:51 -0500

    All set to create APIs

--- db_setup.sql
+++ db_setup.sql
@@ -1,3 +1,15 @@
 create database auth_db;
 
-cr
+create table users (
+	id serial primary key,
+	username varchar(255) unique not null,
+	email varchar(255) unique not null,
+	password_hash text not null,
+	join_date timestamp with time zone not null default current_timestamp, --used to check the time when the user has officially joined the application after verification
+	email_verified boolean default FALSE,
+	verification_token text,
+	last_login timestamp with time zone,
+	created_at timestamp with time zone not null default current_timestamp, --used to check when the user has signed up
+	updated_at timestamp with time zone not null default current_timestamp
+);
+
